c049f56124ba57478a7913698b353c9eee146e2b
Montserrat Monroy <montserratmonroy@MacBook-Pro-de-Montserrat.local>
2018-06-29 15:24:16 -0500

    avance

--- script.js
+++ script.js
@@ -5,24 +5,33 @@ var interval;
 var images = {
     personaje1:'http://vignette4.wikia.nocookie.net/noragami/images/c/cf/Yukine_sin_fondo..png/revision/latest?cb=20150917133821&path-prefix=es',
     personaje2: './personaje2.png',
-    teacher1: './Maestro_Karin.png',
+    teacher: './Maestro_Karin.png',
     //teacher2: './Karin_DB.png',
     //teacher3: './Maestro Karin 2.jpg',
     item1:'./beer.png',
-    item2:'./basura2.png',
-    item3: './platano.png',
-    item4: './cafe.png',
-    item5: './papel.png',
-    item6: './frappe.png',
-    item7: './papel.png',
-    item8:'./laptop3.png',
-    item9:'./platano.png',    
-    gameOver: './failed.jpg',
+    item2:'./cafe.png',
+    item3: './frappe.png',
+    item4: './laptop4.png',
+    item5: './laptop2.png',
+    item6: './teddy bear.png',
+    item7: './cat.png',
+    item8: './dog.png',
+    item9: './huevo2.png',
+    ite10: './basura2.png',
+    item11:'./platano.png',
+    item12:'./papel.png', 
+    item13: './poop happy.png',
+    item14: './manzana.png',
+    item15: './Pet-Bottle-512.png',
+    //score1: './men2.png',
+    score2: './women.png',
+
     bg: 'https://static.wixstatic.com/media/36b7ba_bdfe0078ddf544db9c08b54fded5867c~mv2.jpg'
 }
 
 
-var arrImg = [images.item1, images.item2, images.item3, images.item4, images.item5, images.item6,images.item7, images.item8, images.item9]
+var arrImg1 = [images.item1, images.item2, images.item3,images.item4, images.item5, images.item6, images.item7, images.item8]
+var arrImg2 = [images.item9, images.item10, images.item11,images.item12,images.item13, images.item14, images.item15]
 var frames = 0;
 // var playerOne;
 // var playerTwo;
@@ -32,9 +41,9 @@ var frames = 0;
 var scores = [];
 var counter = 0;
 var items= [];
-//var sound = new Audio();
-//sound.src = './gta-san-andreas-f.mp3',
-//(sound.loop = true);
+var sound = new Audio();
+sound.src = './gta-san-andreas-f.mp3',
+(sound.loop = true);
 
 
 
@@ -53,7 +62,7 @@ class Board{
         }.bind(this)
     }
 
-     youWin(){
+     /*youWin(){
          scores.push(point1);
         ctx.font = "80px cursiva";
         ctx.fillStyle = 'red';
@@ -77,7 +86,7 @@ class Board{
         this.images.onload = function(){
             this.draw;
         }
-    }
+    }*/
    /* gameOver(){
         ctx.font = "80px Avenir";7        ctx.fillText("Game Over", 20,100);
         ctx.font = "20px Serif";
@@ -94,7 +103,7 @@ class Board{
         if(this.x === -this.width) this.x = 0;
         ctx.drawImage(this.image, this.x,this.y,this.width,this.height);
         ctx.drawImage(this.image, this.x,this.y,this.width,this.height);
-        this.youWin.draw;
+        //this.youWin.draw;
         ctx.fillStyle = "white";
         ctx.font = 'bold 50px sans-serif';
         ctx.fillText("IronTrash", 600,60);
@@ -122,21 +131,21 @@ class PlayerOne{
        
     }
     walk(){
-        this.x+=10
+        this.x+=15
         
     }
 
    walkback(){
-    this.x-=20;
+    this.x-=15;
    
 }
 
    moveup(){    
-        this.y-=5 
+        this.y-=15 
     }
 
   movedown(){
-      this.y+=5    
+      this.y+=15    
   }
 
 isTouching(item){
@@ -145,7 +154,6 @@ isTouching(item){
             (this.y < item.y + item.height) &&
             (this.y + this.height > item.y);
   }
-
     draw(){
     
         this.x+1;
@@ -171,23 +179,20 @@ class PlayerTwo{
        
     }
         walk(){
-            if(this.x < (canvas.width-100));
-              this.x+=5;
+            this.x+=15;
          }
 
    walkback(){
-    this.x-=20;
+    this.x-=15;
     
     }
 
    moveup(){
-    if (this.y-=5 < (canvas.height-300));
-             this.y-=5;
+    this.y-=15;
     }
 
   movedown(){
-    (this.y+=10 > (canvas.height-200));
-         this.y+=10;
+    this.y+=15;
     }
 
 isTouching(item){
@@ -196,15 +201,6 @@ isTouching(item){
             (this.y < item.y + item.height) &&
             (this.y + this.height > item.y);
   }
-
-  //changePlayer(playerOne,playerTwo){
-    //    clearInterval(interval);
-      //  interval = undefined;
-        //begin === player2;
-      //}else{
-        //  begin === player1;
-      //}
-  //}
     
 
     draw(){
@@ -238,13 +234,18 @@ class Item{
      }
       draw(){
         
-        if(this.y < (canvas.height-5)) {
+        if(this.y < (canvas.height)) {
             this.y+=this.speed;
-        if(this.x === -this.width) this.x = 0;
+        //if(this.x === -this.width) this.x = 0;
         ctx.drawImage(this.image, this.x,this.y,this.width,this.height);
-    }
+        } else {
+            this.width = undefined;
+            this.height= undefined;
+            this.x = undefined;
+            this.y = undefined;
+        }
     
-}
+    }
 }
         
 class Score{
@@ -254,7 +255,13 @@ class Score{
         this.width = width;
         this.height = height;
         this.score = 0;
-    }
+        this.image = new Image();
+        this.image.src = images.score1;
+        this.image.onload = function(){
+            this.draw();
+        }.bind(this)
+      }
+    
     draw() {
         
         // this.x+=0;
@@ -267,8 +274,21 @@ class Score{
         ctx.fillText(this.score, this.x, this.y )
         //ctx.fillText("Hello World!",10,50);
     }
-    
 }
+
+function draw() {
+    
+    var img = new Image();
+      img.src = "./men.png";
+      // Importante el onload
+      img.onload = function(){
+        ctx.drawImage(img, 390, 680);
+  
+    }
+  }draw()
+
+
+
 
 class Teacher{
         constructor(x, y, width, height){
@@ -277,30 +297,25 @@ class Teacher{
         this.width = width;
         this.height = height;
         this.image = new Image();
-        this.image.src = images.teacher1;
+        this.image.src = images.teacher;
         this.image.onload = function(){
             this.draw();
         }.bind(this);
       }
 
         /*isScreaming(){
-
             if(this.currentTime === 2){
-
                 ctx.drawImage(images.teacher1, this.x,this.y,this.width,this.height);
                 ctx.font = "80px Avenir";
                  ctx.fillText("HURRY UP!!!", 20,100);
             
-
             }else if (this.currentTime === 5){
                 
                 ctx.drawImage(this.image + images.teacher2, this.x,this.y,this.width,this.height);
                 ctx.font = "20px Serif";
                 ctx.fillStyle = 'blue';
                 ctx.fillText("I want my Ironbeer", 20,150);
-
             }else (this.currentTime === 10); {
-
                 ctx.drawImage(this.image + images.teacher3, this.x,this.y,this.width,this.height);
                 ctx.font = "20px Serif";
                 ctx.fillStyle = 'blue';
@@ -315,9 +330,6 @@ class Teacher{
         //this.x+=1;
         //if(this.x === -this.width) this.x = 0;
         ctx.drawImage(this.image, this.x,this.y,this.width,this.height);
-        ctx.fillStyle = "black";
-        ctx.font = 'bold 70px sans-serif';
-        ctx.fillText(this.score, this.x, this.y )
     }
         
 } 
@@ -327,16 +339,17 @@ class Teacher{
 var board = new Board();
 var playerOne = new PlayerOne();
 var playerTwo = new PlayerTwo();
-var point1 = new Score(1000, 50, 100, 100);
-var point2 = new Score(canvas.width-500, 50, 100, 100);
+var point1 = new Score(160, 50, 100, 100);
+var point2 = new Score(canvas.width-200, 50, 100, 100);
 var teacher = new Teacher();
+var score = new Score();
 
 
 //var item = new Item();
 
 //mainFunctions
 function update(){
-    
+    console.log(teacher + "me pinto")
     ctx.clearRect(0,0,canvas.width,canvas.height);  
     frames++; 
     board.draw();
@@ -347,6 +360,8 @@ function update(){
     drawItem();
     teacher.draw()
     generateItem();
+    checkScore();
+   // score1.draw();
     //generateScore(); 
     
       
@@ -368,8 +383,18 @@ function startGame(){
 function generateItem(){
 
     if(!(frames%100===0) ) return;
-    var fotito = arrImg[Math.floor(Math.random()*arrImg.length)]
-    var basurita = new Item(Math.floor(Math.random()*canvas.width),-100, 100, 100,1.3,fotito,1 )
+    var fotito1 = arrImg1[Math.floor(Math.random()*arrImg1.length)]
+    var fotito2 = arrImg2[Math.floor(Math.random()*arrImg2.length)]
+
+    //console.log(Math.floor(Math.random()*8)%2);
+    if (Math.floor(Math.random()*8)%2 === 0){
+        //ES BUENO
+        var basurita = new Item(Math.floor(Math.random()*canvas.width),-100, 100, 100,1.3,fotito2,1);
+    } else {
+        // ES MALO
+        var basurita = new Item(Math.floor(Math.random()*canvas.width),-100, 100, 100,1.3,fotito1,0);    
+    }
+    
     //x,y, width=100, height=100,speed,itemType,goodBad
     items.push(basurita)
     
@@ -434,7 +459,6 @@ function generateScore(){
     var teacher1 = new Teacher(0, 100,canvas.width / 2-350, height,2.3,images.teacher1,2);
     var teacher2 = new Teacher(0, 100, canvas.width / 2 - 150, height,0.5,images.teacher2,5);
     var teacher3 = new Teacher(0, 100, canvas.width / 2, height,1.4,images.teacher3,10);
-
     teachers.push(teacher1);
     teachers.push(teacher2);
     teachers.push(teacher3);
@@ -466,11 +490,11 @@ function drawItem(){
             if(item.goodBad===1){
                 
                 point2.score += 15;
-                //console.log(point1.score)
+                console.log("goodBad bueno="+item.goodBad);
             } else {
                 
                 point2.score -= 10;
-                //console.log(point1.score)
+                console.log("goodBad malo="+item.goodBad);
             } 
 
             items.splice(items.indexOf(item),1);
@@ -487,37 +511,70 @@ function drawItem(){
   //  }
     //      }
 
-function pickWinner(){
-    if (playerOne.x>=canvas.width-300){
+function checkScore(){
+   // console.log('checando', point1.score, point2.score)
+    if (point1.score>=10 ){
+       // console.log(scores.playerOne> scores.playerTwo)
+        gameOver(playerOne)
+    }
+    if (point2.score>=10 ){
+        //console.log(scores.playerOne> scores.playerTwo)
+        gameOver(playerTwo)
+    }
+        // if(playerOne.scores> playerTwo.scores){
+        //     gameOverPlayer1
+        // }else
+        //     gameOverPlayer2
+        // }
+}
+
+function gameOver(player) {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    if (player == playerOne){
+        ctx.fillText('Congratulations!!!' , canvas.width/2-400, canvas.height/2-100);
+        ctx.fillText('Player 1 wins:', canvas.width/2-300, canvas.height/2-50)
+        ctx.fillStyle = "black";
+        ctx.font = 'bold 70px sans-serif';
+    }else if(player == playerTwo){
+        ctx.fillText('Congratulations!!!' , canvas.width/2-400, canvas.height/2-100);
+        ctx.fillText('Player 2 wins:', canvas.width/2-300, canvas.height/2-50)
+    }
+    clearInterval(interval);
+    sound.pause();
+    sound.currentTime = 0;
+    
+    // if(playerOne.scores<playerTwo.scores){
+    //     console.log("The Winner is," + playerOne.scores+ "congratulations");
+    //     clearInterval(interval);
+    //     interval = undefined;
+    //     sound.pause();
+    //     sound.currentTime = 0;
+    //     ctx.clearRect(0,0,canvas.width,canvas.height);
+    // }else
+    //     clearInterval(interval);
+    //     interval = undefined;
+    //     sound.pause();
+    //     sound.currentTime = 0;
+    //     ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+
+function gameOverPlayer2() {
+    if(point2.score < point1.score){
+        console.log("The Winner is," + point2.score+ "congratulations");
         clearInterval(interval);
         interval = undefined;
-        //playerOne.x || playerTwo.x === 5;
-        board.draw();
-        playerOne.draw();
-        //scores.draw();
-        teacher.draw();
         sound.pause();
         sound.currentTime = 0;
         ctx.clearRect(0,0,canvas.width,canvas.height);
-    } else {(playerTwo.x >=canvas.width-200)
+    }else
         clearInterval(interval);
         interval = undefined;
-        //playerOne.x || playerTwo.x === 5;
-        board.draw();
-        playerTwo.draw();
-        //scores.draw();
-       // sound.pause();
-       // sound.currentTime = 0;
+        sound.pause();
+        sound.currentTime = 0;
         ctx.clearRect(0,0,canvas.width,canvas.height);
-    }
-    
-    function finalWinner() {
-    if(playerOne.point1 < playerTwo.point2){
-        ctx.fillText ("Winner, congratulations",300,500)}
-        else (playerTwo.point2 < playerOne.point1)
-        ctx.fillText ("Winner, congratulations",400,600);
-  }
 }
+
 
 
   //function changePlayer() {
@@ -547,10 +604,10 @@ function pickWinner(){
 function restart(){
     if(interval) return;
     items = [];
-    player1.x = 0;
-    player1.y = 0;
-    player2.x = 0;
-    player2.y = 0;
+    playerOne.x = 0;
+    playerOne.y = 0;
+    playerTwo.x = 0;
+    playerTwo.y = 0;
     scores = [];
     teacher.x = 0;
     teacher.y = 0;
